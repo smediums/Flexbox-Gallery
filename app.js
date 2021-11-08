dd01b1fa8b23d3bdbd262f251f6b33614b419e1b
smediums <smediums.me@gmail.com>
2021-11-08 23:20:50 +0300

    Flex Box photo gallery

--- app.js
+++ app.js
@@ -1,55 +1,28 @@
-const time = document.getElementById('time')
-const setTime = document.getElementById('setTime')
-const start = document.getElementById('start')
-const minutes = document.querySelectorAll('.minute')
-const seconds = document.querySelectorAll('.second')
-const secSelector = document.getElementById('theSeconds')
-const minSelector = document.getElementById('minutes')
-let secondNum;
+const items = document.querySelectorAll('.item')
+const view = document.querySelector('.banner')
 
 
-//Give select options a value
-let initMin = -1
-let initSec = -1
-minutes.forEach(minute => {
-    initMin++;
-    minute.innerText = initMin;
-})
-seconds.forEach(second => {
-    initSec++;
-    second.innerText = initSec;
+//Open gallery
+view.addEventListener('click', () => {
+    view.classList.toggle('openGallery')
+
 })
 
 
 
+//Show img
+items.forEach(item => {
+    const upBtn = item.lastElementChild;
+    
+    item.addEventListener('click', () => {
+        items.forEach(itm => {
+            itm.classList.remove('expand')
+        })
+        item.classList.add('expand')
+        
+    })
 
-
-//Timer functionality
-const runTimer = () => {
-    //set input value as initial timer value
-    secondNum = parseInt(secSelector.value) + (parseInt(minSelector.value) * 60);
-
-    //If no input value
-    if(secondNum == ''){
-        alert('Please enter valid time')
-    }else{
-
-    //Run timer
-    const countDown = setInterval(() => {
-    secondNum--;
-        showTime(secondNum)
-        if(secondNum <= 0){
-            clearInterval(countDown);
-        }
-        }, 1000);
-        const showTime = (second) => {
-            const min = Math.floor(second / 60)
-            const sec = Math.floor(second % 60)
-
-            time.innerHTML =`${min < 10 ? '0': ''}${min}:${sec < 10 ? '0': ''}${sec}`
-        }
-    }
-}
-
-//start timer
-start.addEventListener('click', runTimer)
+    upBtn.addEventListener('click', () => {
+        item.classList.toggle('showCaption')
+    })
+})
